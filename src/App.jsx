285ffbdfd5181a31b84f44ceee60f213a9acec69
Joseph Ding <dingyiheng91@gmail.com>
2025-09-07 14:19:32 +0800

    feat: 建立路由结构和layout布局组件

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import HotelListPage from "./pages/HotelListPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* 定义路由 */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/hotels" element={<HotelListPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          {/* 定义路由 */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/hotels" element={<HotelListPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
 
 export default App;
+  
