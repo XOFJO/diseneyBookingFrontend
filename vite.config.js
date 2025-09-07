75d560fbb97375c159324772fa70498729ad3251
Joseph Ding <dingyiheng91@gmail.com>
2025-09-07 12:42:23 +0800

    feat: 加入vite proxy连接后端

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
- 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["diseneybookingfrontend-production.up.railway.app"]
-  }
-})
+    allowedHosts: ["diseneybookingfrontend-production.up.railway.app"],
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://disneybookingbackend-production.up.railway.app",
+        changeOrigin: true,
+        secure: true,
+        // rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+});
