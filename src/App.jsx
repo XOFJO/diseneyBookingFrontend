f1d8fea13abcea7249228b1fb48043e63c15f953
Joseph Ding <dingyiheng91@gmail.com>
2025-09-07 13:31:11 +0800

    chore: 安装tailwind cs s

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-    hello
-    </>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center color">
+      <h1 className="text-4xl font-bold text-red-600">
+        Hello Tailwind CSS!
+      </h1>
+    </div>
   )
 }
 
