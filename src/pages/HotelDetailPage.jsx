bfbdf98f7cb1edcfed0d5bca55e7121676ee37d2
KieranGu <kieran.gu@oocl.com>
2025-09-09 21:20:07 +0800

    fix:满足lint

--- src/pages/HotelDetailPage.jsx
+++ src/pages/HotelDetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import RoomForm from '../components/room/RoomForm'
 import HotelDetailHeader from '../components/room/HotelDetailHeader'
